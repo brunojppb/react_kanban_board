6fd0329530899c8cf75cd4b758e141a6b1e142c4
Bruno Paulino <brunojppb@gmail.com>
2016-08-08 20:38:41 -0300

    changing html attributes like className and comments

--- app/List.js
+++ app/List.js
@@ -3,18 +3,17 @@ import Card from './Card';
 
 class List extends Component {
   render() {
-    // Map the cards to the component
+    {/* Map the cards to the component
     // And pass along the props from the parent
     // to child. That way, React keeps the ownership
-    // of the props to the Parent component
+    // of the props to the Parent component */}
     var cards = this.props.cards.map((card) => {
       return <Card id={card.id}
                     title={card.title}
                     description={card.description}
                     tasks={card.tasks} />
     });
-
-    // Return the List itself with its cards
+    {/* Return the List itself with its cards */}
     return (
       <div className="list">
         <h1>{this.props.title}</h1>
