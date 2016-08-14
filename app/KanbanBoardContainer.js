b4c53322235aa86741da4f3c9531fc8086fefe6a
Bruno Paulino <brunojppb@gmail.com>
2016-08-14 16:30:45 -0300

    deleting from API

--- app/KanbanBoardContainer.js
+++ app/KanbanBoardContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import KanbanBoard from './KanbanBoard';
 import 'whatwg-fetch';
 import 'babel-polyfill'
+import update from 'react-addons-update';
 
 const API_URL = 'http://kanbanapi.pro-react.com';
 const API_HEADERS = {
@@ -35,6 +36,24 @@ class KanbanBoardContainer extends Component {
 
   removeTask(cardId, taskId, taskIndex) {
     console.log('removed task');
+    // Find the index of the card
+    let cardIndex = this.state.cards.findIndex((card) => card.id == cardId);
+
+    // Create a new object without the task that will be deleted
+    let nextState = update(this.state.cards, {
+      [cardIndex]: {
+        tasks: { $splice: [[taskIndex, 1]] }
+      }
+    });
+
+    //set the component state to the mutated object
+    this.setState({ cards: nextState });
+
+    // Call the API to remove the task on the server
+    fetch(`${API_URL}/cards/${cardId}/tasks/${taskId}`, {
+      method: 'delete',
+      headers: API_HEADERS
+    });
   }
 
   toggleTask(cardId, taskId, taskIndex) {
