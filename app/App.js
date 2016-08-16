75d945764938c63cd5d9f420d00b5cb8f5e4b046
Bruno Paulino <brunojppb@gmail.com>
2016-08-16 10:29:13 -0300

    CSS Animations

--- app/App.js
+++ app/App.js
@@ -1,5 +1,56 @@
 import React, { Component } from 'react';
-import KanbanBoardContainer from './KanbanBoardContainer';
 import { render } from 'react-dom';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-render(<KanbanBoardContainer />, document.getElementById('root'));
+class AnamitedShoppingList extends Component {
+  constructor() {
+    super(...arguments);
+    // Create items state
+    this.state = {
+      items: [
+        { id: 1, name: 'Milk' },
+        { id: 2, name: 'Yogurt' },
+        { id: 3, name: 'Orange Juice' }
+      ]
+    };
+  }
+
+  handleChange(event) {
+    if(event.key === 'Enter') {
+      let newItem = { id: Date.now(), name: event.target.value };
+      let newItems = this.state.items.concat(newItem);
+      event.target.value = "";
+      this.setState({items: newItems});
+    }
+  }
+
+  handleRemove(i) {
+    var newItems = this.state.items;
+    newItems.splice(i, 1);
+    this.setState({ items: newItems });
+  }
+
+  render() {
+    let shoppingItems = this.state.items.map((item, i) => (
+      <div key={item.id} className="item" onClick={this.handleRemove.bind(this, i)}>
+        {item.name}
+      </div>
+    ));
+
+    return (
+      <div>
+        <ReactCSSTransitionGroup transitionName="example"
+                                  transitionEnterTimeout={300}
+                                  transitionLeaveTimeout={300}
+                                  transitionAppear={true}
+                                  transitionAppearTimeout={300}>
+          {shoppingItems}
+        </ReactCSSTransitionGroup>
+        <input type="text" value={this.state.newItem} onKeyDown={this.handleChange.bind(this)}/>
+      </div>
+    )
+  }
+
+}
+
+render(<AnamitedShoppingList />, document.getElementById('root'));
