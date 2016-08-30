cbd4c192ce292538b6096ea2409d91e3eef3f093
Bruno Paulino <brunojppb@gmail.com>
2016-08-30 09:33:07 -0300

    rendering routes

--- app/About.js
+++ app/About.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 
-class About extends Component { render() {
-return ( <h1>ABOUT</h1>
-); }
+class About extends Component {
+
+  render() {
+    return (
+      <h1>ABOUT</h1>
+    );
+  }
+  
 }
 
 export default About;
--- app/App.js
+++ app/App.js
@@ -1,45 +1,51 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Router, Route, Link } from 'react-router';
 
 import About from './About';
 import Home from './Home';
 import Repos from './Repos';
 
 class App extends Component {
-  constructor() {
-    super(...arguments);
-    this.state = {
-      route: window.location.hash.substr(1)
-    };
-  }
-
-  componentDidMount() {
-    window.addEventListener('hashchange', () => {
-      this.setState({
-        route: window.location.hash.substr(1)
-      });
-    });
-  }
 
   render() {
-    var Child;
-    switch (this.state.route) {
-      case '/about': Child = About; break;
-      case '/repos': Child = Repos; break;
-      default: Child = Home;
-    }
-
     return(
       <div>
         <header>App</header>
         <menu>
-          <ul><a href="#/about">About</a></ul>
-          <ul><a href="#/repos">Repos</a></ul>
+          <ul>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/repos">Repos</Link>
+            </li>
+          </ul>
         </menu>
-        <Child />
+        {this.props.children}
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render((
+  <Router>
+    <Route path="/" component={App}>
+      <Route path="about" component={About} />
+      <Route path="repos" component={Repos} />
+    </Route>
+  </Router>
+), document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
+
+//
